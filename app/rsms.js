03b98bbaff3568958b348d39bc163353e7b42e96
Giulio Meneghin <bluemana@gmail.com>
2016-08-13 15:34:25 +0700

    Add WebSocket connection

---
+++ app/rsms.js
@@ -0,0 +1,5 @@
+/* global io */
+
+(function() {
+  io.connect();
+}());
--- server/rsms.js
+++ server/rsms.js
@@ -1,11 +1,19 @@
 const bunyan = require("bunyan");
 const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 
 const LOGGER = bunyan.createLogger({name: "rsms"});
 const APP_DIR = __dirname.substr(0, __dirname.lastIndexOf("/")) + "/app";
 LOGGER.info("Static files directory: " + APP_DIR);
 
-const server = express();
-server.use(express.static(APP_DIR));
+const expressApp = express();
+expressApp.use(express.static(APP_DIR));
 
-module.exports.server = server;
+const httpServer = http.createServer(expressApp);
+const webSocketApp = socketIo.listen(httpServer);
+webSocketApp.on("connection", function() {
+  LOGGER.info("WebSocket connected");
+});
+
+module.exports.server = httpServer;
