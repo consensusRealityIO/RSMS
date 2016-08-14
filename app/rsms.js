eee3c9fa27aec5f5500b2d20226d9a1068592bdf
Giulio Meneghin <bluemana@gmail.com>
2016-08-14 14:05:44 +0700

    Display error message if connection error

--- app/rsms.js
+++ app/rsms.js
@@ -1,5 +1,13 @@
-/* global io */
+/* global io $*/
 
 (function() {
-  io.connect();
+  var webSocket = io.connect();
+  webSocket.on("disconnect", function() {
+    $(".status-row").removeClass("invisible");
+    $(".status-content").text("Connection error");
+  });
+  webSocket.on("connect", function() {
+    $(".status-row").addClass("invisible");
+    $(".status-content").text("");
+  });
 }());
